ed9446d3821674841cbc074bcc1a26d617a96781
Magnum4518 <lethalneurotoxin@gmail.com>
2025-04-07 17:00:10 -0400

    Create window.h

---
+++ src/window.h
@@ -0,0 +1,32 @@
+#include <string.h>
+
+#include <SFML/Graphics.hpp>
+
+#ifndef CMAKESFMLPROJECT_WINDOW_H
+#define CMAKESFMLPROJECT_WINDOW_H
+
+#endif //CMAKESFMLPROJECT_WINDOW_H
+
+class Window
+{
+private:
+    unsigned int width;
+    unsigned int height;
+    std::string title;
+    sf::VideoMode vm;
+    sf::RenderWindow *window;
+
+    void initWindow();
+
+    void pollEvents();
+
+    void updating();
+    void rendering();
+
+
+public:
+    Window();
+    ~Window();
+
+    void running();
+};
